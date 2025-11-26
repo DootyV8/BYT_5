7609bafd94c91035262103560768f7674d5f81db
DootyV8 <s30515@pjwstk.edu.pl>
2025-11-26 18:47:18 +0100

    Everything done

---
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -0,0 +1,24 @@
+﻿// In case you need some guidance: https://refactoring.guru/design-patterns/adapter
+namespace DesignPattern.Adapter
+{
+    public class EmployeeAdapter : ITarget
+    {
+        private readonly BillingSystem _billingSystem = new();
+        
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            List<Employee> employeeList = new List<Employee>();
+            
+            for (int i = 0; i < employeesArray.GetLength(0); i++)
+            {
+                int id = int.Parse(employeesArray[i, 0]);
+                string name = employeesArray[i, 1];
+                string designation = employeesArray[i, 2];
+                decimal salary = decimal.Parse(employeesArray[i, 3]);
+
+                employeeList.Add(new Employee(id, name, designation, salary));
+            }
+            _billingSystem.ProcessSalary(employeeList);
+        }
+    }
+}
---
+++ DesignPattern/Observer/Observer.cs
@@ -0,0 +1,22 @@
+﻿// In case you need some guidance: https://refactoring.guru/design-patterns/observer
+
+namespace DesignPattern.Observer
+{
+    public class ConcreteObserver(string userName) : IObserver
+    {
+        public string UserName { get; private set; } = userName;
+        public void Update(string availability)
+        {
+            Console.WriteLine("Hello " + UserName + ", Product is now " + availability);
+        }
+        public void AddSubscriber(ISubject subject)
+        {
+            subject.RegisterObserver(this);
+        }
+        public void RemoveSubscriber(ISubject subject)
+        {
+            subject.RemoveObserver(this);
+        }
+    }
+}
+
